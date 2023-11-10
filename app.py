93aa74c597b951ad19fb05ba3fe0fb54ea12788f
Gabriel Fernandes de Freitas Moreira Duarte de Lima <gabrielfernandes2005@gmail.com>
2023-11-10 14:12:06 -0300

    123

--- app.py
+++ app.py
@@ -1,6 +1,21 @@
-from Flask import Flask, render_template, request, redirect, url_for, flash
+from flask import Flask, render_template, request, redirect, url_for, flash
 
 app = Flask(__name__)
+
+
+@app.route("/cadastro")
+def cadastro():
+    return render_template("cadastro.html")
+
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+
+@app.route("/")
+def inicio():
+    return redirect(url_for("cadastro"))
 
 
 if __name__ == "__main__":
